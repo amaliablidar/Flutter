2c7f1edba51c6f93e70985f59999528e6471174a
amaliablidar <91790692+amaliablidar@users.noreply.github.com>
2021-11-26 00:11:46 +0200

    Add files via upload

---
+++ Recipes-App/lib/screens/favorites_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/meal.dart';
+import 'package:flutter_complete_guide/widgets/meal_item.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text(
+          'You have no favorites yet - start adding some!',
+          textAlign: TextAlign.center,
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+            duration: favoriteMeals[index].duration,
+            imageUrl: favoriteMeals[index].imageUrl,
+            title: favoriteMeals[index].title,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
